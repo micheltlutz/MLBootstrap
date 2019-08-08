336f69ce31fbdf5c377de7e3c5be2274d795e2f7
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2019-08-07 21:38:31 -0300

    Adjusts ok

---
+++ Sources/Core/Skins/MLImages.swift
@@ -0,0 +1,9 @@
+//
+//  MLImages.swift
+//  MLBootstrap-iOS
+//
+//  Created by Michel Anderson Lutz Teixeira on 06/08/19.
+//  Copyright © 2019 micheltlutz. All rights reserved.
+//
+
+import Foundation
