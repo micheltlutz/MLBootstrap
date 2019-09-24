015ab058d8d8edf956771e625c60807ab7f006c1
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2019-09-23 23:13:45 -0300

    Phone Formatter

--- Sources/Core/Extensions/String/String+Extensions.swift
+++ Sources/Core/Extensions/String/String+Extensions.swift
@@ -16,4 +16,88 @@ extension String {
     public mutating func capitalizeFirstLetter() {
         self = self.capitalizingFirstLetter()
     }
+
+    public var westernArabicNumeralsOnly: String {
+        let pattern = UnicodeScalar("0")..."9"
+        return String(unicodeScalars
+            .compactMap { pattern ~= $0 ? Character($0) : nil })
+    }
+
+    public var stringByRemovingWhitespaces: String {
+        let components = self.components(separatedBy: .whitespaces)
+        return components.joined(separator: "")
+    }
+
+    public var withoutCharactersAndSpace: String {
+        var text = self.replacingOccurrences(of: ",", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: "-", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: "(", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: ")", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: " ", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: "+", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: ":", with: "", options: .literal, range: nil)
+        text = text.trimmingCharacters(in: .whitespaces)
+        text = text.stringByRemovingWhitespaces
+        return text
+    }
+
+    public var withoutSpecialCharacters: String {
+        var text = self.replacingOccurrences(of: ",", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: "-", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: "(", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: ")", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: "+", with: "", options: .literal, range: nil)
+        text = text.replacingOccurrences(of: ":", with: "", options: .literal, range: nil)
+        return text
+    }
+
+    public func withoutSpecialCharacters(andSpace: Bool = false) -> String {
+        var possibleCharacters = Set("aáâãbcdeéêfghiíjklmnoóôõpqrstuúüvwxyzAÁÂÃBCDEÉÊFGHIÍJKLKMNOÓÔÕPQRSTUÚÜVWXYZ1234567890")
+
+        if !andSpace {
+            possibleCharacters.insert(" ")
+        }
+
+        return String(self.filter { possibleCharacters.contains($0) })
+    }
+
+    public func removeDDI() -> String {
+        let phone = self
+        if phone.count == 13 {
+            let phoneWithoutDDI = String(phone.dropFirst(2))
+
+            return phoneWithoutDDI
+        } else {
+            return phone
+        }
+    }
+
+    public var digitsOnly: String {
+        return components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+    }
+
+    public var hasOnlyDigits: Bool {
+        return rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+    }
+
+    public var hasOnlyLetters: Bool {
+        return rangeOfCharacter(from: CharacterSet.letters.inverted) == nil
+    }
+
+    public var lettersOnly: Bool {
+        let onlyLetters: (Character) -> Bool = { ("a"..."z" ~= $0) || ("A"..."Z" ~= $0) }
+        return allSatisfy(onlyLetters)
+    }
+
+    public func trimmingTrailingSpaces() -> String {
+        var trim = self
+        while trim.hasSuffix(" ") {
+            trim = "" + trim.dropLast()
+        }
+        return trim
+    }
+
+    public mutating func trimmedTrailingSpaces() {
+        self = self.trimmingTrailingSpaces()
+    }
 }
